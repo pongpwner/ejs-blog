33b7de20b644169c4ffba90fa3c5180d788a3c60
pongpwner <ericsshyus@gmail.com>
2022-03-17 00:44:27 -0400

    finished challenge

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const { application } = require("express");
+var _ = require("lodash");
+
+// Load the FP build for immutable auto-curried iteratee-first data-last methods.
+var fp = require("lodash/fp");
+
+// Load method categories.
+var array = require("lodash/array");
+var object = require("lodash/fp/object");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -39,6 +47,16 @@ app.post("/compose", (req, res) => {
   const post = { title: req.body.title, content: req.body.body };
   posts.push(post);
   res.redirect("/");
+});
+
+app.get("/posts/:postName", (req, res) => {
+  posts.forEach((post) => {
+    if (_.lowerCase(req.params.postName) === _.lowerCase(post.title)) {
+      res.render("post", { title: post.title, content: post.content });
+    } else {
+      console.log("not a match");
+    }
+  });
 });
 ///////////////
 app.listen(3000, function () {
